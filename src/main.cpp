e3b6c8aa91ef1fe0511bc81b8d0c40ad98efe01e
DreamMaoMao <2523610504@qq.com>
2024-02-18 12:22:47 +0800

    disable workspace change in ov mode

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle)
 	CONF("swipe_fingers", int, 4);
 	CONF("move_focus_distance", int, 100);
 	CONF("enable_gesture", int, 0);
-	CONF("disable_workspace_change", int, 0);
+	CONF("disable_workspace_change", int, 1);
 	CONF("disable_spawn", int, 0);
 	CONF("auto_exit", int, 1);
 	CONF("auto_fullscreen", int, 0);
